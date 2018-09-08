87ad39d3d6807872927cfb2b52441a3d297d4b89
Heather Cleland <heathercleland@gmail.com>
2018-09-08 16:40:10 -0500

    refactor using each method

---
+++ Question-1.rb
@@ -0,0 +1,23 @@
+# Read about the Ruby `each` method for arrays. Then refactor the code below using `each`.
+
+numbers = [1, 2, 4, 2]
+sum = 0
+index = 0
+
+numbers.length.times do
+  number = numbers[index]
+  sum = sum + number
+  index = index + 1
+end
+p sum
+
+numbers = [1, 2, 4, 2]
+sum = 0
+index = 0
+
+numbers.each do |number|
+  sum = sum + number
+end
+p sum
+
+
