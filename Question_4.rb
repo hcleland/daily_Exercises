1fe32d9f3078c7e86f672486e27477a1bb362ab4
Heather Cleland <heathercleland@gmail.com>
2018-09-10 13:37:47 -0500

    break while loop with correct user input

---
+++ Question_4.rb
@@ -0,0 +1,24 @@
+# The following code will run 100 times, and each time it will ask the user for their name. The code will break early if the user’s name is Bob.
+
+# 100.times do
+#   puts "What is your name?"  
+#   name = gets.chomp
+#   if name == "Bob"
+#     break
+#   end
+# end
+
+# puts "Hi, Bob!"
+
+# Rewrite the code using a while loop so the program will run *forever* unless the user enters a name of Bob.
+
+puts "What is your name?"
+
+while name = gets.chomp 
+  if name == "Bob" || name == "bob"
+    break
+  end
+  puts "What is your name?"
+end
+
+puts "Hi, Bob!"
---
+++ Question_5.rb
@@ -0,0 +1,4 @@
+# Write a method called `average` that takes in an array of numbers and returns the average (the sum divided by the total number of numbers). Write it first using a `while` loop, then write it again using an `each` loop. (Hint - if you’re having issues with missing decimals, you may need the .to_f method…)
+
+# p average([2, 1, 7, 5])  #=> 3.75
+
