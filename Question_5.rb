512692d83cbc23eba50afa6efc8a5f62f467cda8
Heather Cleland <heathercleland@gmail.com>
2018-09-10 14:47:55 -0500

    write average method using while and each

--- Question_5.rb
+++ Question_5.rb
@@ -2,3 +2,26 @@
 
 # p average([2, 1, 7, 5])  #=> 3.75
 
+def average(array)
+  index = 0
+  sum = 0
+  while index < array.length
+    sum += array[index]
+    index += 1 
+  end
+  sum = sum / array.length.to_f
+end
+
+p average([17,23,33,42,58])
+
+
+def average(array)
+  index = 0
+  sum = 0
+  array.each do |number|
+    sum += number
+  end
+  sum / array.length.to_f
+end
+
+p average([3,7,12,16])
